a42e4218bce007e5c594bd3490dadc8e27032a91
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-05-05 23:53:25 +0530

    added consumer

---
+++ java_stream/ConsumerInterface.java
@@ -0,0 +1,17 @@
+package java_stream;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class ConsumerInterface {
+    public static void main(String[] args) {
+        Consumer<Integer> c1 = c -> System.out.println(c);
+        BiConsumer<String, String> c2 = (key, value) -> System.out.println(key+ " "+value);
+        Arrays.asList(1,2,3).forEach(c1);
+
+        Map.of("key1", "value1", "key2", "value2").forEach(c2);
+    }
+    
+}
