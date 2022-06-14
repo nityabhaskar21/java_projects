ff871e53d8a43342e182f53fad5db3cbcf3073df
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-06-14 23:05:35 +0530

    Added zoned date time and git ignore

---
+++ java_time/ZoneTime.java
@@ -0,0 +1,14 @@
+package java_time;
+
+import java.time.ZonedDateTime;
+
+/**
+ * ZoneTime
+ */
+public class ZoneTime {
+
+    public static void main(String[] args) {
+        ZonedDateTime z1 = ZonedDateTime.now();
+        System.out.println("ZonedDateTime1: " + z1);
+    }
+}
