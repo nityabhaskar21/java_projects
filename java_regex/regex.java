9992896b0bb4b0ff12017fd4cc12ff37435c0321
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-06-24 10:21:33 +0530

    Added comments for regex

--- java_regex/regex.java
+++ java_regex/regex.java
@@ -3,6 +3,13 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.Scanner;
 
+// Lookarounds
+// Lookaround	Legend	Example	Sample Match
+// (?=…)	Positive lookahead	(?=\d{10})\d{5}	01234 in 0123456789
+// (?<=…)	Positive lookbehind	(?<=\d)cat	cat in 1cat
+// (?!…)	Negative lookahead	(?!theatre)the\w+	theme
+// (?<!…)	Negative lookbehind	\w{3}(?<!mon)ster	Munster
+
 
 public class regex {
     public static void main(String[] args) {
