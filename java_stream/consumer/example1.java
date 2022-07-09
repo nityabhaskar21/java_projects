3e70862458a1aee0e6d3e5f31deef5e5d502d2c6
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-07-09 12:45:40 +0530

    Added consumer example 1

---
+++ java_stream/consumer/example1.java
@@ -0,0 +1,28 @@
+package java_stream.consumer;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class example1 {
+    public static void main(String[] args) {
+
+		List<Integer> list = List.of(34,67,8,23,67,89,90);
+		
+		Consumer<Integer> consumer = System.out :: println ;
+		
+		consumer.accept(56);
+		
+		printElements(list, consumer);
+		
+	}
+
+	private static <T> void printElements(List<T> list, Consumer<T> consumer) {
+		
+		for (T t : list) {
+			
+			consumer.accept(t);
+			
+		}
+		
+	}
+}
