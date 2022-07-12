4b20f114613bad3f12e8bbebc80b8e3359ccb5f5
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2022-07-12 19:41:53 +0530

    Added stream on map 2 examples.

---
+++ java_stream/stream_on_map/GetMapKeys.java
@@ -0,0 +1,19 @@
+package java_stream.stream_on_map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GetMapKeys {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+
+        map.put("name", "abc");
+        map.put("class", "four");
+        map.put("job", "student");
+        map.put("father", "xyz");
+
+        var list = map.entrySet().stream().map(m -> m.getKey()).collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
