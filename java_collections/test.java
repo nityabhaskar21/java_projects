cedc9aeec03888671190ef67538e9346117ed4a5
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-09-16 17:46:08 +0530

    Updated

---
+++ java_collections/test.java
@@ -0,0 +1,16 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+
+    public static void main(String[] args) {
+        List<String> l = new ArrayList<>();
+        l.add("ugu");
+        l.add("gug");
+    
+        System.out.println(l.toString());
+    }
+    
+}
