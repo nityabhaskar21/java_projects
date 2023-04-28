67f96df4414e234014ff6b786c94ec5d99cf69bf
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2023-04-28 11:06:50 +0530

    Commit Uuid generator

--- java_new_features/UuidGenerator.java
+++ java_new_features/UuidGenerator.java
@@ -5,27 +5,17 @@ import java.util.UUID;
 
 public class UuidGenerator {
     public static void main(String[] args) {
-
-        String providerAccountId= "730963009792";
-        
-        String cloudAccountId= "60c8293912f6f518065c2c3f";
-        
-        String region ="ap-northeast-1";
-        
-        var uuid =generateUUIDToken(cloudAccountId,providerAccountId,region);
-        
-        System.out.println("uuid: "+ uuid.toString());
-        
+            String providerAccountId= "730963009792";
+            String cloudAccountId= "63f4af61a833f3758ccfb4ac";
+            String region ="ap-south-1";
+            var uuid =generateUUIDToken(cloudAccountId,providerAccountId,region);
+            System.out.println("uuid: "+ uuid.toString());
         }
         
         
         public static UUID generateUUIDToken(String cloudAccountId, String providerAccountId, String region){
-        
-        String combine = String.format("%s:-:%s:-:%s", cloudAccountId, providerAccountId, region);
-        
-        byte[] byteArray = combine.getBytes(StandardCharsets.UTF_8);
-        
-        return UUID.nameUUIDFromBytes(byteArray);
-        
+            String combine = String.format("%s:-:%s:-:%s", cloudAccountId, providerAccountId, region);
+            byte[] byteArray = combine.getBytes(StandardCharsets.UTF_8);
+            return UUID.nameUUIDFromBytes(byteArray);
         }
 }
