bf52aff199525013afde12a2d4368508c566f9b3
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2023-05-08 18:07:37 +0530

    Updated uuid token

--- java_new_features/UuidGenerator.java
+++ java_new_features/UuidGenerator.java
@@ -4,18 +4,22 @@ import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class UuidGenerator {
+
     public static void main(String[] args) {
-            String providerAccountId= "730963009792";
-            String cloudAccountId= "63f4af61a833f3758ccfb4ac";
-            String region ="ap-south-1";
-            var uuid =generateUUIDToken(cloudAccountId,providerAccountId,region);
-            System.out.println("uuid: "+ uuid.toString());
-        }
-        
-        
-        public static UUID generateUUIDToken(String cloudAccountId, String providerAccountId, String region){
-            String combine = String.format("%s:-:%s:-:%s", cloudAccountId, providerAccountId, region);
-            byte[] byteArray = combine.getBytes(StandardCharsets.UTF_8);
-            return UUID.nameUUIDFromBytes(byteArray);
-        }
+
+        String cloudAccountId= "63db49a1a3a9b62cd66a312b";
+        String providerAccountId= "757333951934";
+        String region ="ap-south-1";
+
+        var uuid =generateUUIDToken(cloudAccountId,providerAccountId,region);
+        // var uuid =generateUUIDToken(providerAccountId,cloudAccountId,region);
+        System.out.println("uuid: "+ uuid.toString());
+    }
+    
+    
+    public static UUID generateUUIDToken(String cloudAccountId, String providerAccountId, String region){
+        String combine = String.format("%s:-:%s:-:%s", cloudAccountId, providerAccountId, region);
+        byte[] byteArray = combine.getBytes(StandardCharsets.UTF_8);
+        return UUID.nameUUIDFromBytes(byteArray);
+    }
 }
