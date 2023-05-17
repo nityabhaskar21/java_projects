175f55a02d9050bfb874d2a9108927db7afac3b4
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2023-05-17 21:34:23 +0530

    Updated

--- employee_in_out/Test.java
+++ employee_in_out/Test.java
@@ -5,43 +5,49 @@ import java.text.*;
 
 public class Test {
     public static void main(String[] args) {
+        BufferedReader reader;
         try {
-            File file = new File("E:\\java_projects\\employee_in_out\\data.txt");
-            BufferedReader br= new BufferedReader(new FileReader(file));
-            String st="";
+            reader = new BufferedReader(new FileReader("E:\\java_projects\\employee_in_out\\data.txt"));
+			String st = reader.readLine();
             String id="";
             String time="";
             String type="";
             Map<String,Map<String,Object>> map=new HashMap<>();
-            while ((st = br.readLine()) != null)
+            while (st != null)
             {
                 id=st.split(",")[0];
                 time=st.split(",")[1];
                 type=st.split(",")[2];
 
                 DateFormat formatter = new SimpleDateFormat("hh:mm:ss");
-                System.out.println("id: "+id+"time "+time+"type: "+type);
+                System.out.println("id:"+id+", time:"+time+", type:"+type);
                 Date time_date = formatter.parse(time);
 
                 // convert time in a standard format
                 if(!map.containsKey(id)){
-                    map.put(id, Map.of("last_time",time_date,"entry_type","IN", "odc",0.0, "no_of_entries", 0));
+                    map.put(id, Map.of("last_time",time_date,"entry_type","IN", "odc",0l, "no_of_entries", 0));
                 } else {
                     if (type.equals("OUT")) {
-                        var totalOdc=time_date.getTime()-((Date)(map.get(id).get("last_entry_time"))).getTime() +((Date) map.get(id).get("odc")).getTime();
+                        HashMap<String, Object> tmpMap =new HashMap<>();
+                        tmpMap.putAll(map.get(id));
+                        var totalOdc=time_date.getTime()-((Date)(map.get(id).get("last_time"))).getTime() + (long)map.get(id).get("odc");
                         int currTimes=(int)map.get(id).get("no_of_entries")+1;
-                        map.get(id).put("odc", totalOdc);
-                        map.get(id).put("entry_type",type);
-                        map.get(id).put("last_time", time_date);
-                        map.get(id).put("no_of_entries", currTimes);
+                        tmpMap.put("odc", totalOdc);
+                        tmpMap.put("entry_type",type);
+                        tmpMap.put("last_time", time_date);
+                        tmpMap.put("no_of_entries", currTimes);
+                        map.put(id, tmpMap);
                     } else {
-                        map.get(id).put("entry_type",type);
-                        map.get(id).put("last_time", time_date);
+                        HashMap<String, Object> tmpMap =new HashMap<>();
+                        tmpMap.putAll(map.get(id));
+                        tmpMap.put("entry_type",type);
+                        tmpMap.put("last_time", time_date);
+                        map.put(id, tmpMap);
                     }
                 }   
+                st = reader.readLine();
             }
-            br.close();
-            
+
             map.forEach((k, v) -> {
                 System.out.print(k+", ");
                 if (v.get("entry_type").equals("IN")) {
@@ -53,6 +59,7 @@ public class Test {
             });
             
         } catch(Exception e) {
+            e.printStackTrace();
                 System.out.println(e.getMessage());
             }
     }
