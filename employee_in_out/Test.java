61f5d478d2e55c29236ed20628c10169ec1d9be5
Nityanand Bhaskar <nityabhaskar21@gmail.com>
2023-05-17 21:52:57 +0530

    Added code

--- employee_in_out/Test.java
+++ employee_in_out/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader("E:\\java_projects\\employee_in_out\\data.txt"));
+            reader = new BufferedReader(new FileReader("F:\\coding\\java_projects\\employee_in_out\\data.txt"));
 			String st = reader.readLine();
             String id="";
             String time="";
@@ -31,17 +31,20 @@ public class Test {
                         HashMap<String, Object> tmpMap =new HashMap<>();
                         tmpMap.putAll(map.get(id));
                         var totalOdc=time_date.getTime()-((Date)(map.get(id).get("last_time"))).getTime() + (long)map.get(id).get("odc");
+                        System.out.println("totalOdc"+totalOdc);
                         int currTimes=(int)map.get(id).get("no_of_entries")+1;
                         tmpMap.put("odc", totalOdc);
                         tmpMap.put("entry_type",type);
                         tmpMap.put("last_time", time_date);
                         tmpMap.put("no_of_entries", currTimes);
+                        System.out.println(tmpMap);
                         map.put(id, tmpMap);
                     } else {
                         HashMap<String, Object> tmpMap =new HashMap<>();
                         tmpMap.putAll(map.get(id));
                         tmpMap.put("entry_type",type);
                         tmpMap.put("last_time", time_date);
+                        System.out.println(tmpMap);
                         map.put(id, tmpMap);
                     }
                 }   
@@ -49,13 +52,17 @@ public class Test {
             }
 
             map.forEach((k, v) -> {
-                System.out.print(k+", ");
+                System.out.print("id:"+k+", ");
                 if (v.get("entry_type").equals("IN")) {
-                System.out.print(v.get("no_of_entries"+"+"+", "));
+                System.out.print("no_of_entries:"+v.get("no_of_entries")+"+");
                 } else {
-                System.out.print(v.get("no_of_entries"+", "));
+                System.out.print("no_of_entries:"+v.get("no_of_entries"));
                 }
-                System.out.println(v.get("odc"));
+                var odc=Math.abs((long)v.get("odc"));
+                int seconds = (int) ((long)odc / 1000) % 60 ;
+                int minutes = (int) (((long)odc / (1000*60)) % 60);
+                int hours   = (int) (((long)odc / (1000*60*60)) % 24);
+                System.out.println(", hours:" + hours+" minutes:" + minutes);
             });
             
         } catch(Exception e) {
