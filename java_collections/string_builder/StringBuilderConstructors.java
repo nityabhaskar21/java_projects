49f80c7741a4d3ef7cf695243c47bef03c616a55
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2024-01-10 17:46:52 +0530

    Added StringBuilder methods

---
+++ java_collections/string_builder/StringBuilderConstructors.java
@@ -0,0 +1,24 @@
+package java_collections.string_builder;
+
+public class StringBuilderConstructors {
+    public static void main(String[] args) {
+        
+        StringBuilder ob1 = new StringBuilder();
+        //This constructor is used for creating a StringBuilder object with no characters in it 
+        //and initial capacity of 16 characters.
+
+        StringBuilder ob2 = new StringBuilder(10);
+        //This constructor is used for creating a StringBuilder object with no characters in it 
+        //and an initial capacity specified by the capacity argument.
+
+        CharSequence seq = "Scaler";
+        StringBuilder sb3 = new StringBuilder(seq);
+        //This constructor is used for creating a StringBuilder object with initial characters 
+        //specified by the CharSequence argument seq.
+
+
+        StringBuilder sb4 = new StringBuilder("Scaler");
+        //This constructor is used for creating a StringBuilder object with 
+        //initial String content the same as str characters
+    }
+}
---
+++ java_collections/string_builder/StringBuilderMethods.java
@@ -0,0 +1,46 @@
+package java_collections.string_builder;
+
+public class StringBuilderMethods {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello ");
+
+        //append
+        StringBuilder sb2=sb.append("Java");//now original string is also changed  
+        System.out.println(sb);//prints Hello Java  
+        System.out.println(sb2);//prints Hello Java  
+
+        //charAt
+        System.out.println(sb.charAt(1));//e
+
+        //delete
+        StringBuilder sb3 = sb.delete(1,3);  //also we have sb.delete(2) i.e. single index.
+        System.out.println(sb);//Hlo Java  
+        System.out.println(sb2);//Hlo Java , Since sb2 also pointing to same object
+        System.out.println(sb3);//Hlo Java 
+
+        sb=new StringBuilder("Hello ");
+
+        //replace
+        sb2=sb.replace(1,3,"Java");  //including index 1 but excluding index 3.
+        System.out.println(sb);//prints HJavalo      
+        System.out.println(sb2);//prints HJavalo     
+
+        sb=new StringBuilder("Hello ");
+
+        //insert
+        sb2=sb.insert(1,"Java");//now original string is also changed  
+        System.out.println(sb);//prints HJavaello 
+        System.out.println(sb2);//prints HJavaello 
+
+        sb=new StringBuilder("Hello ");
+
+        //reverse
+        sb2=sb.reverse();  
+        System.out.println(sb);//prints " olleH" 
+        System.out.println(sb2);//prints " olleH" 
+
+        //length
+        System.out.println(sb.length());
+
+    }
+}
