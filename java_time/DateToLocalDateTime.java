b07505300bd6e6efc0a4a1f72de753177db7eceb
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2024-03-14 17:24:31 +0530

    Added date parser

--- java_time/DateToLocalDateTime.java
+++ java_time/DateToLocalDateTime.java
@@ -1,8 +1,11 @@
 package java_time;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateToLocalDateTime {
     public static void main(String[] args) {
@@ -18,6 +21,16 @@ public class DateToLocalDateTime {
         //LocalDateTime to Date
         Date d1 = Date.from(l.atZone(ZoneId.systemDefault()).toInstant());
         System.out.println("Date: " + d1);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        String dateInString = "2024-03-14T12:14:48.855+01:00"; 
+        try {
+            Date date = dateFormat.parse(dateInString);
+            System.out.println(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
