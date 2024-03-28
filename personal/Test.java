46a8bcb5c948e63a999f2306f3c75a8dadcf891c
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2024-03-28 17:44:29 +0530

    Updated project structure

--- personal/Test.java
+++ personal/Test.java
@@ -2,6 +2,6 @@ package personal;
 
 public class Test {
     public static void main(String[] args) {
-        
+        System.out.println("Organization deactivation not allowed for org: AAA-MC-TestORG.\nReasons:\n- Active resources exists. Please delete the resources.\r\n- Active Cloud Subscription exists. Please delete the orders.\r\n");
     }
 }
