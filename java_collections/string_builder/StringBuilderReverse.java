de9d93154dd528622b4bc775dfe85a15fa021fbe
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2024-04-05 12:24:19 +0530

    Added StringBuilder reverse.

---
+++ java_collections/string_builder/StringBuilderReverse.java
@@ -0,0 +1,11 @@
+package java_collections.string_builder;
+
+public class StringBuilderReverse {
+    public static void main(String[] args) {
+        StringBuilder sb1=new StringBuilder("Hello World");
+        StringBuilder sb2=sb1.reverse();
+
+        System.out.println(sb1);    //dlroW olleH
+        System.out.println(sb2);    //dlroW olleH
+    }
+}
