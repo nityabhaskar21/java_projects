8f1b3b50bf3bed9e7be15a07bf3b2588f0d2f623
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2024-04-22 15:08:41 +0530

    Added int char to int

---
+++ java_collections/character/IntegerCharToInt.java
@@ -0,0 +1,10 @@
+package java_collections.character;
+
+public class IntegerCharToInt {
+    public static void main(String[] args) {
+        char char1 = '7';
+        int num1=char1 - '0';
+        System.out.println(char1 - '0');
+        System.out.println(num1+1);
+    }
+}
